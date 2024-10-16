cbd70d3f54089c963addef739cbe9f678158bfc3
Gavin Grochowski <grochowskigavin@gmail.com>
2024-10-16 00:46:59 -0700

    Add brainstorming questions with Ell, and question selection.

---
+++ src/prompts.py
@@ -0,0 +1,129 @@
+from typing import Optional, List
+from random import randint
+
+import ell
+
+
+def get_quality_icebreaker_questions(
+    n_seeded_questions: int = 5, previously_asked_questions: Optional[List[str]] = None
+) -> List[str]:
+    """
+    Generate 3 quality icebreaker questions.
+
+    Parameters
+    ----------
+    n_seeded_questions : int, optional
+        Number of questions to initially prune down from, by default 5
+    previously_asked_questions : Optional[List[str]], optional
+        Text of previously genearated questions to avoid, by default None
+
+    Returns
+    -------
+    str
+        Question Text, generated.
+    """
+
+    initial_questions = brainstorm_icebreaker_questions(
+        n_questions=n_seeded_questions,
+        previous_questions_text=(
+            "\n".join(previously_asked_questions)
+            if previously_asked_questions
+            else None
+        ),
+    )
+    best_questions = select_3_best_questions(initial_questions)
+    plain_questions = plainify_questions(best_questions)
+    return extract_questions(plain_questions)
+
+
+@ell.simple(model="gpt-4o-mini", temperature=1.3)
+def brainstorm_icebreaker_questions(
+    n_questions: int, previous_questions_text: Optional[str] = None
+):
+    """
+    You are a researcher helping design questions to create relationships
+    between new friends.
+    Your subjects are two classmates that sit next to each other and spend lots
+    of time together, but are NOT close to each other emotionally.
+    There is an awkward silence between them,
+    but they both want to get to know each other.
+
+    You should NOT speak like "chatgpt",
+    but instead you should sound like a casual professor teaching a course
+    to put your test subjects at ease.
+    """
+    initial_prompt = (
+        f"Can you ask {n_questions} 'icebreaker' questions, "
+        "each designed to spark a conversation "
+        "to fill the downtime before the next class begins for the two classmates? "
+        f"Each of the {n_questions} questions should be more intimate "
+        "and personal than the last question, "
+        "with question number 1 being a fairly comfortable question "
+        f"and question number {n_questions} being fairly prying. "
+        "Be sure to separate each question with square brackets, for example [Question?]. "
+        "Do not include anything other than the questions in the square brackets."
+    )
+    if not previous_questions_text:
+        return initial_prompt
+
+    follow_up_prompt = (
+        f"Great! Please ask {n_questions} more 'icebreaker' questions in the same way, "
+        "each different from the previously asked questions. "
+    )
+    return [
+        ell.user(initial_prompt),
+        ell.assistant(previous_questions_text),
+        ell.user(follow_up_prompt),
+    ]
+
+
+@ell.simple(model="gpt-4o-mini", temperature=1.0)
+def plainify_questions(
+    questions_text: str,
+) -> str:
+    """
+    You and I are classmates with 5 minutes of down time before our
+    next class.
+    You should NOT speak like "chatgpt",
+    but instead you should sound like a normal girl
+    taking a post-secondary university course with me.
+    """
+    return (
+        "Here are a few icebreaker questions I heard from a friend. "
+        "Each of them is a question enclosed in square brackets, like [Question?]. "
+        "But like, they are pretty formal, and I think a chatbot came up with them. "
+        "Can you help me out and ask them to me in plain language like we'd normally use in class? "
+        "I'd love for you to add your own style and put your own spin on them as well! "
+        "It'd still be super helpful if you could put the questions in square brackets like they came in, "
+        " and not say anything else other than the questions. "
+        f"Here are the questions: \n {questions_text}"
+    )
+
+
+@ell.simple(model="gpt-4o-mini", temperature=1.0)
+def select_3_best_questions(
+    questions_text: str,
+) -> str:
+    """
+    You are the teenage chaparone for your two best friends on a date.
+    Your two best friends don't know each other very well, but they want to get to know each other.
+    There is an awkward silence between them, and they need you to ask them a question.
+    You want to help them show vulnarability to each other, but not too much vulnarability
+    that they will be uncomfortable and afraid of being judged.
+    """
+    return (
+        "Here are a few icebreaker questions I heard from a friend. "
+        "Each of them is a question enclosed in square brackets, like [Question?]. "
+        "But like, some of them might be too boring, and some of them might be too scary for two best friends on a date. "
+        "Could you help select the 3 most interesting questions that would be ideal for breaking the tension on the date? "
+        "It'd still be super helpful if you could put the questions in square brackets like they came in, "
+        " and not say anything else other than the questions. "
+        f"Here are the questions: \n {questions_text}"
+    )
+
+
+def extract_questions(question_text) -> list:
+    """Extract [square enclosed] questions from text_string"""
+    question_strings = question_text.split("[")
+    questions = [q.strip().strip("]") for q in question_strings[1:]]
+    return questions
